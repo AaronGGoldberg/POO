b52dec82739e2b82e9e417338ede2cef1710f4bf
AaronGGoldberg <aaron.guerra137@gmail.com>
2025-04-10 17:28:57 +0000

    Recursividade Exemplo

--- Aula-02/PythonC++C#Java/InputC#/obj/Debug/net8.0/Input#.AssemblyInfo.cs
+++ Aula-02/PythonC++C#Java/InputC#/obj/Debug/net8.0/Input#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Input#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47d641be44d1c190920210de7d9359d9ebc7eb34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4abf5da7d03d5493c100381424c50d41c1c10452")]
 [assembly: System.Reflection.AssemblyProductAttribute("Input#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Input#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
