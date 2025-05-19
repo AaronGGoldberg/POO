5e3698d93201439f6701a010fd6833a86b02a6b0
AaronGGoldberg <aaron.guerra137@gmail.com>
2025-05-19 21:06:53 +0000

    Dicionários em Arquivos

---
+++ Dicionarios-Arquivos/Ex01.py
@@ -0,0 +1,15 @@
+
+import enum
+
+class Estacao(enum.Enum):
+    OUTONO = 1
+    INVERNO = 2
+    PRIMAVERA = 3
+    VERAO = 4
+    
+a = Estacao.INVERNO
+b = Estacao["VERAO"]
+c = Estacao(3)
+print(a) # Estacao.INVERNO
+print(b) # Estacao.VERAO
+print(c) # Estacao.PRIMAVERA
