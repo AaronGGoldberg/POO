43751f5a6bed78024f52f4752bb090822bc73b26
AaronGGoldberg <aaron.guerra137@gmail.com>
2025-06-12 14:45:51 +0000

    Avaliação Final sobre Projeto de Comércio Eletrônico Completo

--- Aula_01-02/PythonC++C#Java/InputC#/obj/Debug/net8.0/Input#.AssemblyInfo.cs
+++ Aula_01-02/PythonC++C#Java/InputC#/obj/Debug/net8.0/Input#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Input#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f84b98371b6bef91bccfe8278d8ac329a4307e43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6414e082de11607fba326de8d230b36e052000")]
 [assembly: System.Reflection.AssemblyProductAttribute("Input#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Input#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
