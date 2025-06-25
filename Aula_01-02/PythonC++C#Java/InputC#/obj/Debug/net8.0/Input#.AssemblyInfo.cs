df48f1f96700e56d1860cd951b47dae223555825
AaronGGoldberg <aaron.guerra137@gmail.com>
2025-06-25 17:20:39 +0000

    Aula01

--- Aula_01-02/PythonC++C#Java/InputC#/obj/Debug/net8.0/Input#.AssemblyInfo.cs
+++ Aula_01-02/PythonC++C#Java/InputC#/obj/Debug/net8.0/Input#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Input#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5dc60b5a60c4423aff4c3f68829a6b7db4787e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8af49cf9f85e2e2453821f4faadda017dfe1652")]
 [assembly: System.Reflection.AssemblyProductAttribute("Input#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Input#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
