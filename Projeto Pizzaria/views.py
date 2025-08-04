10e7b2203f88cb4a6b3dc17f5d3d9ece94ae42eb
AaronGGoldberg <aaron.guerra137@gmail.com>
2025-08-04 04:30:56 +0000

    Projeto Pizzaria em andamento

---
+++ Projeto Pizzaria/views.py
@@ -0,0 +1,7 @@
+from Models.cliente import Cliente
+from Models.produto import Produto
+from Models.pedido import Pedido
+from Models.entregador import Entregador
+from Models.usuario import Usuario
+from Models.itemPedido import ItemPedido
+from Models.categoria import Categoria
