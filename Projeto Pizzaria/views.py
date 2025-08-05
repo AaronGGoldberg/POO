6a6048250eb63c9a7dace6d5b0af4345c4aeb9b6
AaronGGoldberg <aaron.guerra137@gmail.com>
2025-08-05 17:57:30 +0000

    Projeto Final de Pizzaria com StreamLit concluído com sucesso

--- Projeto Pizzaria/views.py
+++
@@ -1,7 +0,0 @@
-from Models.cliente import Cliente
-from Models.produto import Produto
-from Models.pedido import Pedido
-from Models.entregador import Entregador
-from Models.usuario import Usuario
-from Models.itemPedido import ItemPedido
-from Models.categoria import Categoria
